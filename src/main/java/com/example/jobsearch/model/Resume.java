00e8c7e82e466774b3cc7e08bb649c9f5562e96b
Алексей <khvan_leha@mail.ru>
2025-07-11 16:17:18 +0600

    add variables Resume

--- src/main/java/com/example/jobsearch/model/Resume.java
+++ src/main/java/com/example/jobsearch/model/Resume.java
@@ -1,4 +1,18 @@
 package com.example.jobsearch.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
 public class Resume {
+    private int id;
+    private int applicantId;
+    private String name;
+    private int categoryId;
+    private double salary;
+    private boolean isActive;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateTime;
 }
