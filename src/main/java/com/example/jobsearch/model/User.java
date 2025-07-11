6c7c735fd72aec5547e36167938b0b7052483e0e
Алексей <khvan_leha@mail.ru>
2025-07-11 16:15:47 +0600

    add info User

--- src/main/java/com/example/jobsearch/model/User.java
+++ src/main/java/com/example/jobsearch/model/User.java
@@ -1,4 +1,19 @@
 package com.example.jobsearch.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String avatar;
+    private String accountType;
 }
