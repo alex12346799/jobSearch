9ab8c0cf5e47cc7a58f6e71d593b880565f86720
Алексей <khvan_leha@mail.ru>
2025-07-13 23:17:03 +0600

    add class VacancyStorage

--- src/main/java/com/example/jobsearch/model/Vacancy.java
+++ src/main/java/com/example/jobsearch/model/Vacancy.java
@@ -6,14 +6,14 @@ import java.time.LocalDateTime;
 @Data
 public class Vacancy {
     private int id;
-    private String name;
+    private String title;
     private String description;
     private int categoryId;
     private double salary;
     private int expFrom;
     private int expTo;
     private boolean isActive;
-    private int authorId;
+    private int employerId;
     private LocalDateTime createdDate;
     private LocalDateTime updateTime;
 }
