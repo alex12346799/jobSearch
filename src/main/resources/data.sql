aa7d85f3b9842856b68d457af042da61e6aeb3ce
Алексей <khvan_leha@mail.ru>
2025-07-16 17:47:54 +0600

    filled the tables with data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,64 @@
 create table if not exists users
 (
-                       id BIGINT PRIMARY KEY,
-                       name VARCHAR(100),
-                       surname VARCHAR(100),
-                       role VARCHAR(50),
-                       age INT,
-                       email VARCHAR(100) UNIQUE,
-                       password VARCHAR(100),
-                       phone_number VARCHAR(20),
-                       address VARCHAR(255),
-                       avatar VARCHAR(255),
-                       account_type VARCHAR(50)
+    id           INT PRIMARY KEY,
+    name         VARCHAR(100),
+    surname      VARCHAR(100),
+    role         VARCHAR(50),
+    age          INT,
+    email        VARCHAR(100) UNIQUE,
+    password     VARCHAR(100),
+    phone_number VARCHAR(20),
+    address      VARCHAR(255),
+    avatar       VARCHAR(255),
+    account_type VARCHAR(50)
 );
-INSERT INTO users (id, name, surname, role, age, email, password, phone_number, address, avatar, account_type)
-VALUES
-    (1, 'Иван', 'Иванов', 'APPLICANT', 25,
-     'ivan@example.com', '1234', '0555123456', 'г. Бишкек',
-     null, 'BASIC'),
-    (2, 'ОАО Альфа', 'HR', 'EMPLOYER', 40,
-     'hr@alfa.kg', 'admin', '0700111222',
-     'г. Бишкек, ул. Советская', null, 'BUSINESS');
+
 
 create table if not exists resume
 (
-    id           int auto_increment primary key,
-    applicant_Id  int,
+    id           INT AUTO_INCREMENT PRIMARY KEY,
+    applicant_Id int,
     name         VARCHAR(100),
     category_id  INT,
     salary       DOUBLE,
     is_active    BOOLEAN,
     created_date TIMESTAMP,
-    update_time  TIMESTAMP
+    update_time  TIMESTAMP,
+    FOREIGN KEY (applicant_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
 
 );
-INSERT INTO resume (id, applicant_id, name, category_id, salary, is_active, created_date, update_time)
-VALUES (1, 1, 'Java Developer', 1, 120000, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       (2, 1, 'Frontend Developer', 1, 100000, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+create table if not exists category
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(45),
+    parent_id INT
+);
+
+CREATE TABLE vacancy
+(
+    id           INT PRIMARY KEY,
+    title        VARCHAR(100),
+    description  TEXT,
+    category_id  INT,
+    salary       DOUBLE,
+    exp_from     INT,
+    exp_to       INT,
+    is_active    BOOLEAN,
+    employer_id  INT,
+    created_date TIMESTAMP,
+    update_time  TIMESTAMP,
+    FOREIGN KEY (employer_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+CREATE TABLE respondent_applicant
+(
+    id           INT PRIMARY KEY,
+    resume_id    INT,
+    vacancy_id   INT,
+    confirmation BOOLEAN,
+    create_date  TIMESTAMP,
+    FOREIGN KEY (resume_id) REFERENCES resume(id),
+    FOREIGN KEY (vacancy_id) REFERENCES vacancy(id)
+);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (id, name, surname, role, age, email, password, phone_number, address, avatar, account_type)
+VALUES
+    (1, 'Иван', 'Иванов', 'APPLICANT', 25,
+     'ivan@example.com', '1234', '0555123456', 'г. Бишкек',
+     null, 'BASIC'),
+    (2, 'ОАО Альфа', 'HR', 'EMPLOYER', 40,
+     'hr@alfa.kg', 'admin', '0700111222',
+     'г. Бишкек, ул. Советская', null, 'BUSINESS');
+
+INSERT INTO resume (id, applicant_id, name, category_id, salary, is_active, created_date, update_time)
+VALUES (1, 1, 'Java Developer', 1,
+        120000, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       (2, 1, 'Frontend Developer', 1,
+        100000, true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO category (id, name, parent_id) VALUES
+                                               (1, 'IT', null),
+                                               (2, 'Маркетинг', null);
+
+INSERT INTO vacancy (id, title, description, category_id, salary, exp_from, exp_to, is_active, employer_id, created_date, update_time)
+VALUES
+    (1, 'Junior Java Developer', 'Работа в команде разработки',
+     1, 110000, 0, 2, true, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'Маркетолог', 'Поиск и привлечение клиентов',
+     2, 90000, 1, 3, true, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO respondent_applicant (id, resume_id, vacancy_id, confirmation, create_date)
+VALUES
+    (1, 1, 1, false, CURRENT_TIMESTAMP);
