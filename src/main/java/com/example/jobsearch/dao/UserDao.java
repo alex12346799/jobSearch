edcc06042f286ff112d1ec3b28305bed253ede0e
Алексей <khvan_leha@mail.ru>
2025-07-17 13:16:52 +0600

    add class userDao

--- src/main/java/com/example/jobsearch/dao/UserDao.java
+++ src/main/java/com/example/jobsearch/dao/UserDao.java
@@ -1,21 +1,46 @@
 package com.example.jobsearch.dao;
 
 import com.example.jobsearch.model.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
+
+
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
+@Repository
+
 public class UserDao {
     private final JdbcTemplate jdbcTemplate;
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    public List<User> getAllUsers(){
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<User> findByName(String name) {
+        String sql = "SELECT * FROM users where name = ?";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class), name);
+    }
+
+
+    public User findByEmail(String email) {
+        String sql = "SELECT * FROM users WHERE email = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), email);
+    }
+
+    public List<User> findByPhone(String phone) {
+        String sql = "SELECT * FROM users WHERE PHONE_NUMBER = ?";
+        return  jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class),phone);
+    }
+    public List<User> findAll() {
         String sql = "SELECT * FROM users";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public boolean existsByEmail(String email) {
+        String sql = "SELECT COUNT(*) FROM users WHERE email = ?";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, email);
+        return count != null && count > 0;
     }
 }
