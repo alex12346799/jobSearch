a92ddfe5ddc042291824c1ed3369b7f092d9c1be
Алексей <khvan_leha@mail.ru>
2025-07-23 18:17:39 +0600

    add class GlobalControllerEdvice

---
+++ src/main/java/com/example/jobsearch/exceptions/GlobalControllerEdvice.java
@@ -0,0 +1,15 @@
+package com.example.jobsearch.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerEdvice {
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorResponse handleNotFoundException(NotFoundException ex) {
+        return ErrorResponse.builder(ex, HttpStatus.NOT_FOUND, ex.getMessage()).build();
+    }
+}
